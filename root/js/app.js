733c30e9ae4ed444ff84f0e673546991879e822a
Rodrigo Moguillanksy <romoguill@gmail.com>
2022-06-04 15:59:24 -0300

    Added functionality that allows to store the id from a movie on the localStorage

--- root/js/app.js
+++ root/js/app.js
@@ -11,17 +11,27 @@ const addToWatchlistBtnFunctionality = () => {
 };
 
 const addToWatchlist = (e) => {
-  const imdbId = e.target.dataset.imdbid;
+  const imdbId = e.target.closest('[data-imdbid]').dataset.imdbid;
+  if (!watchlist.includes(imdbId)) {
+    watchlist.push(imdbId);
+  }
+
+  localStorage.setItem('watchlist', JSON.stringify(watchlist));
 };
 
-const watchlist = localStorage.getItem('storedMovies') || [];
+const watchlist = JSON.parse(localStorage.getItem('watchlist')) || [];
 
 // Listen for a title search, search for the data and render it
 formElement.addEventListener('submit', async (e) => {
   const formData = new FormData(formElement);
   const titleSearched = formData.get('title');
 
+  // Call to API and store the returned object
   const moviesData = await getData(titleSearched);
+
+  // Insert HTML template to create movies card
   renderMovies(moviesData);
+
+  // Give functionality to the watchlist button
   addToWatchlistBtnFunctionality();
 });
