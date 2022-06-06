ee0f4efbe6927045580fbbff4764fe2d4b65adab
romoguill <romoguill@gmail.com>
2022-06-06 07:47:49 -0300

    Added loadAndRenderWatchlist function which renders the watchlist. Also calls the addRemoveBtnFunctionality that allows to remove a movie from the watchlist

--- root/js/watchlist.js
+++ root/js/watchlist.js
@@ -1,15 +1,34 @@
 import { getDataFromWatchlist } from './api-interface.js';
 import { renderMovies } from './render.js';
 
+// Load the actual watchlist to a variable
 const watchlist = JSON.parse(localStorage.getItem('watchlist')) || [];
 
+// Gets the data from the API, then renders it and finally add functionality to the remove btn
 const loadAndRenderWatchlist = async () => {
   try {
     const moviesData = await getDataFromWatchlist(watchlist);
     renderMovies(moviesData, null);
+    addRemoveBtnFunctionality();
   } catch (error) {
     console.log(error);
   }
+};
+
+// Listens for clicks on remove btn and removes that item form watchlist and saves it to local storage.
+//  Excecutes a re-render of the hole page.
+const addRemoveBtnFunctionality = () => {
+  const removeBtnElements = document.querySelectorAll('.btn-watchlist');
+  console.log(removeBtnElements);
+  removeBtnElements.forEach((btn) =>
+    btn.addEventListener('click', (e) => {
+      const imdbId = e.target.closest('[data-imdbid]').dataset.imdbid;
+      const index = watchlist.indexOf(imdbId);
+      watchlist.splice(index, 1);
+      localStorage.setItem('watchlist', JSON.stringify(watchlist));
+      loadAndRenderWatchlist();
+    })
+  );
 };
 
 loadAndRenderWatchlist();
